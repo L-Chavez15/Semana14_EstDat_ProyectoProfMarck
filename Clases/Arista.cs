59f48570df4d483a6aca9db5015b3e5fb7507b56
Leonardo Chávez <n00449821pn.pe>
2026-06-23 17:34:30 -0500

    creacion de aristas, segunda fase del proyecto

---
+++ Clases/Arista.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    //Arista = NODO
+    public class Arista
+    {
+        public Vertice destino;
+
+        public float peso;
+
+        //lista simple
+        public Arista sig = null;
+
+    }
+}
--- Clases/Vertice.cs
+++ Clases/Vertice.cs
@@ -14,10 +14,11 @@ namespace Clases
         public Vertice sig = null;
 
         //grafo
-        public Vertice arista1 = null;
+        /*public Vertice arista1 = null;
         public Vertice arista2 = null;
         public Vertice arista3 = null;
         public Vertice arista4 = null;
-        public Vertice arista5 = null;
+        public Vertice arista5 = null;*/
+        listaArista la = new listaArista();
     }
 }
